358a9f7613e61c6cdd33c7f56cc7e221c6258e44
genwarhunter <kostyarik98@mail.ru>
2017-03-31 11:45:25 +0500

    Update graph.cpp

--- graph/graph.cpp
+++ graph/graph.cpp
@@ -31,6 +31,12 @@ int main()
 	printf("%d\n", color);
 	for (int i = 0; i < size; i++)
 		printf("%d ", buffer[i]);
+	for (int i = 0; i<size; i++)
+	{
+		free(graph[i]);
+	}
+	free(graph);
+	free(buffer);
     return 0;
 }
 
